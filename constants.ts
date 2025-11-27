ca14edf4fa1aeaf241fa9be8d1d49af05b357b94
bacalhau92 <siljrg92@gmail.com>
2025-11-27 02:09:03 +0000

    Add constants.ts via Deployly

---
+++ constants.ts
@@ -0,0 +1,19 @@
+
+import { SystemSettings } from './types';
+
+export const DEFAULT_SETTINGS: SystemSettings = {
+  adminWhatsapp: "+244931802367",
+  openingHours: "Aberto das 18h às 0h",
+  isSessionActive: false,
+  popularSongs: [
+    { title: 'Evidências', artist: 'Chitãozinho & Xororó' },
+    { title: 'Bohemian Rhapsody', artist: 'Queen' },
+    { title: 'Anna Júlia', artist: 'Los Hermanos' },
+    { title: 'Cheia de Manias', artist: 'Raça Negra' },
+    { title: 'Dormi na Praça', artist: 'Bruno & Marrone' },
+    { title: 'Wonderwall', artist: 'Oasis' },
+    { title: 'It\'s My Life', artist: 'Bon Jovi' },
+    { title: 'I Will Survive', artist: 'Gloria Gaynor' },
+    { title: 'Tempo Perdido', artist: 'Legião Urbana' },
+  ]
+};
